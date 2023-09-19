e6141b92341b7899a82282aeb1acaabb6286d238
Michael Liu <5413532+michaelangeliu@users.noreply.github.com>
2023-09-19 11:09:29 -0500

    Replace minigrep search for loops with iterator filter
    - iterators abstract away the looping and state management allowing us
    to focus more on the high-level objective

--- rust/projects/minigrep/src/lib.rs
+++ rust/projects/minigrep/src/lib.rs
@@ -51,28 +51,23 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase(); // to_lowercase handles basic Unicode and will create a String rather than a string slice (original string may not contain the appropriate lowercase letters)
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) { // needs the & because contains takes a string slice
-            results.push(line);
-        }
-    }
-    
-    results
+    contents
+        .lines()
+        .filter(|line| {
+            line
+                .to_lowercase()
+                .contains(&query) // needs the & because contains takes a string slice
+        })
+        .collect()
 }
 
 #[cfg(test)]
